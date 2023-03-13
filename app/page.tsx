d809d5c89da155de88542ff18594ab552cc9d43e
Marii Odynska <mvasylenko21@gmail.com>
2023-03-13 14:44:31 +0200

    Add Dashboard component

--- app/page.tsx
+++ app/page.tsx
@@ -1,11 +1,14 @@
 import Sidebar from "@/components/Sidebar";
+import Dashboard from "@/components/Dashboard";
 
 import styles from "./styles.module.scss";
 
-export default function Home() {
+const Home = () => {
   return (
     <main className={styles.main_page}>
       <Sidebar />
+      <Dashboard />
     </main>
   );
-}
+};
+export default Home;
---
+++ components/Dashboard/Dashboard.tsx
@@ -0,0 +1,8 @@
+import React from "react";
+import styles from "./styles.module.scss";
+
+const Dashboard: React.FC = () => {
+  return <div>This is Dashboard Component</div>;
+};
+
+export default Dashboard;
