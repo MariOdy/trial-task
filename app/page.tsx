d13ee0639ea4ed17016ee5061e0acc24806f5ad5
Marii Odynska <mvasylenko21@gmail.com>
2023-03-13 16:02:53 +0200

    Complete Sidebar component

--- app/page.tsx
+++ app/page.tsx
@@ -2,6 +2,9 @@ import Sidebar from "@/components/Sidebar";
 import Dashboard from "@/components/Dashboard";
 
 import styles from "./styles.module.scss";
+import "@/styles/reset.css";
+import "@/styles/styles.css";
+import "@/styles/colors.css";
 
 const Home = () => {
   return (
--- components/Sidebar/Sidebar.tsx
+++ components/Sidebar/Sidebar.tsx
@@ -1,9 +1,27 @@
 import React from "react";
+import Link from "next/link";
+import Image from "next/image";
+import logo from "public/inc-icon.png";
+import { BiUser } from "react-icons/bi";
+import { IoSettingsOutline } from "react-icons/io5";
 
 import styles from "./styles.module.scss";
 
-const Sidebar: React.FC = () => {
-  return <div>This is Sidebar</div>;
+const Sidebar = () => {
+  return (
+    <div className={styles.sidebar_wrapper}>
+      <Link href="#">
+        <Image src={logo} alt="logo of half of the moon" />
+      </Link>
+      <span />
+      <Link href="#">
+        <BiUser />
+      </Link>
+      <Link href="#">
+        <IoSettingsOutline opacity={0.5} />
+      </Link>
+    </div>
+  );
 };
 
 export default Sidebar;
